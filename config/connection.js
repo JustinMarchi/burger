c7afb8c2fd3514a69ae6442419e12fc362472568
Justin Marchi <marchij86@gmail.com>
2020-05-09 19:05:13 -0400

    Tweaking connection

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,27 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
+const connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Vermont#1",
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "burgers_db"
   });
 };
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: 3306,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
