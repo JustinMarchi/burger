74a92c9ecfdf7dfacd4b9683674e2d8aa4dd86e4
Justin Marchi <marchij86@gmail.com>
2020-05-09 19:11:23 -0400

    Successfully created Heroku connection, hiding connection credentials

--- config/connection.js
+++ config/connection.js
@@ -15,14 +15,6 @@ if(process.env.JAWSDB_URL) {
   });
 };
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: 3306,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: "burgers_db"
-// });
-
 // Make connection.
 connection.connect(function(err) {
   if (err) {
